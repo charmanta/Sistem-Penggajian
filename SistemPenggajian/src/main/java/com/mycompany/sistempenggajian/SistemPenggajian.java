9129c5152d1b92065dbc0df20588685221b37203
Salsabila <nabilacintabumi@gmail.com>
2026-06-14 13:25:49 +0700

    Menambahkan project Sistem Penggajian

---
+++ SistemPenggajian/src/main/java/com/mycompany/sistempenggajian/SistemPenggajian.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistempenggajian;
+
+/**
+ *
+ * @author User
+ */
+public class SistemPenggajian {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginAdmin().setVisible(true);
+            }
+        });
+    }
+}
