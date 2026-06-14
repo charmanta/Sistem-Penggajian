352339c4b9c8e657448dac92a85b0a3a250451e7
charmanta <charmantaardiva@gmail.com>
2026-06-14 15:22:42 +0700

    Add files via upload

---
+++ penggajian/src/main/java/com/mycompany/penggajian/DBconnection.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.penggajian;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.sql.Connection;
+ import java.sql.DriverManager;
+ import java.sql.SQLException;
+
+public class DBconnection {
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+        // cek apakah koneksi sudah ada
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://127.0.0.1:3306/penggajian";
+                String user = "root";
+                String password = "";
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Koneksi Berhasil!");
+            } catch (SQLException t) {
+                System.out.println("Error Membuat Koneksi: " + t.getMessage());
+            } catch (ClassNotFoundException ex) {
+                System.getLogger(DBconnection.class.getName())
+                    .log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
+        return koneksi;
+    }
+}
+    
+
